cbf7bcedc7f46905eedeb3c9439a2e1cb78ee7d4
podamone <papipoda99@gmail.com>
2024-11-17 16:17:50 +0530

    Update Clone.py

--- nexichat/modules/Clone.py
+++ nexichat/modules/Clone.py
@@ -84,10 +84,10 @@ async def clone_txt(client, message):
         except BaseException as e:
             logging.exception("Error while cloning bot.")
             await mi.edit_text(
-                f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @THE_VIP_BOY_OP for assistance**"
+                f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @tscoa for assistance**"
             )
     else:
-        await message.reply_text("**Provide Bot Token after /clone Command from @Botfather.**")
+        await message.reply_text("**Provide Bot Token after /clone Command from @Botfather otherwise mgs @tscoa.**")
 
 
 @app.on_message(filters.command("cloned"))
