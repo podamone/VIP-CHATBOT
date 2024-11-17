e31a3b4cb1997d3a4c406f31624ad4ceb1dfa3f4
podamone <papipoda99@gmail.com>
2024-11-17 16:57:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "28105090"
+API_ID = "6435225"
 # -------------------------------------------------------------
-API_HASH = "4de697ff186d365497af2de4ce8fb3ed"
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
