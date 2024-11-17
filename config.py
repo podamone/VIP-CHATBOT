2b783e7c263ff4e5e64b423073a274616b954c81
podamone <papipoda99@gmail.com>
2024-11-17 16:40:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "28105090"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "4de697ff186d365497af2de4ce8fb3ed"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = int(getenv("OWNER_ID", "7506257487"))
+SUPPORT_GRP = "https://t.me/+26BfaIqHA3s4YWU1"
+UPDATE_CHNL = "https://t.me/+26BfaIqHA3s4YWU1"
+OWNER_USERNAME = "Hibamolll"
